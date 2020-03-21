07f92bbd2a0dbfdca6e9cd85246077af9c9c1c90
Adrian <ah@clickbar.rocks>
2020-03-21 14:36:20 +0100

    Fixed missing corners of bottom sheet

--- lib/widgets/encounter_bottom_sheet_title.dart
+++ lib/widgets/encounter_bottom_sheet_title.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_contact_tracker/routes/contact_search/selected_contacts/selected_contacts_bloc.dart';
@@ -6,18 +5,23 @@ import 'package:social_contact_tracker/routes/contact_search/selected_contacts/s
 import 'flat_round_icon_button.dart';
 
 class EncounterBottomSheetTitle extends SliverPersistentHeaderDelegate {
-
   static const HEIGHT = 61.0;
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Container(
-      color: Colors.white,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
       child: Row(
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.only(
-                left: 16, top: 16, bottom: 16),
+            padding: const EdgeInsets.only(left: 16, top: 16, bottom: 16),
             child: Row(
               children: <Widget>[
                 Text(
@@ -29,20 +33,17 @@ class EncounterBottomSheetTitle extends SliverPersistentHeaderDelegate {
                   ),
                 ),
                 const SizedBox(width: 8),
-                BlocBuilder<SelectedContactsBloc,
-                    SelectedContactsState>(
+                BlocBuilder<SelectedContactsBloc, SelectedContactsState>(
                     condition: (s1, s2) =>
-                    s2 is ContactInsertState ||
-                        s2 is ContactRemovedState,
+                        s2 is ContactInsertState || s2 is ContactRemovedState,
                     builder: (context, state) {
-                      final count = BlocProvider.of<
-                          SelectedContactsBloc>(context)
-                          .contacts
-                          .length;
+                      final count =
+                          BlocProvider.of<SelectedContactsBloc>(context)
+                              .contacts
+                              .length;
                       return Container(
                         decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color(0xFF2D3748)),
+                            shape: BoxShape.circle, color: Color(0xFF2D3748)),
                         height: 28,
                         width: 28,
                         child: Center(
@@ -63,11 +64,9 @@ class EncounterBottomSheetTitle extends SliverPersistentHeaderDelegate {
           Spacer(),
           Padding(
             padding: const EdgeInsets.only(right: 16.0),
-            child: BlocBuilder<SelectedContactsBloc,
-                SelectedContactsState>(
+            child: BlocBuilder<SelectedContactsBloc, SelectedContactsState>(
               condition: (s1, s2) =>
-              s2 is NoContactsSelectedState ||
-                  s2 is ContactsSelectedState,
+                  s2 is NoContactsSelectedState || s2 is ContactsSelectedState,
               builder: (context, state) {
                 return FlatRoundIconButton(
                   child: Text('Hinzufügen'),
@@ -75,11 +74,9 @@ class EncounterBottomSheetTitle extends SliverPersistentHeaderDelegate {
                     Icons.add,
                     color: Colors.white,
                   ),
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16, vertical: 8),
-                  onTap: state is NoContactsSelectedState
-                      ? null
-                      : () {},
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  onTap: state is NoContactsSelectedState ? null : () {},
                 );
               },
             ),
@@ -97,5 +94,4 @@ class EncounterBottomSheetTitle extends SliverPersistentHeaderDelegate {
 
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => false;
-
 }
