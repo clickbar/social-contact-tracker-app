1656ceeb058c3d33c9460017b1426e48ee5a43da
Adrian <ah@clickbar.rocks>
2020-03-23 00:24:15 +0100

    Fixed UI issues in contact sync setup

--- lib/routes/setup/contact_sync_setup_screen.dart
+++ lib/routes/setup/contact_sync_setup_screen.dart
@@ -14,24 +14,24 @@ class ContactSyncSetupScreen extends StatelessWidget {
         builder: (context) => Scaffold(
           backgroundColor: Color(0xFF2B6CB0),
           body: SafeArea(
-            child: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(height: 64),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Hero(
-                        tag: 'sign_in_logo',
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 2 / 6,
-                          child: Image.asset('assets/images/logbook_logo.png'),
-                        ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 64),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Hero(
+                      tag: 'sign_in_logo',
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 2 / 6,
+                        child: Image.asset('assets/images/logbook_logo.png'),
                       ),
                     ),
-                    BlocBuilder<ContactSyncBloc, ContactSyncState>(
+                  ),
+                  Expanded(
+                    child: BlocBuilder<ContactSyncBloc, ContactSyncState>(
                       builder: (context, state) {
                         if (state is ContactPermissionRequiredState) {
                           return Container(
@@ -39,19 +39,25 @@ class ContactSyncSetupScreen extends StatelessWidget {
                                 horizontal: 24, vertical: 24),
                             child: Center(
                               child: Column(
-
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
-                                  const SizedBox(height:80),
+                                  Text(
+                                    'Fast geschafft',
+                                    style: TextStyle(
+                                        fontSize: 40,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 32),
                                   Text(
                                     'Damit du einfach Begegnungen hinzufügen kannst, müssen wir auf die Kontakte in deinem Adressbuch zugreifen.',
-                                    textAlign: TextAlign.center,
                                     style: TextStyle(
                                         color: Colors.white70,
-                                        fontSize: 18,
+                                        fontSize: 16,
                                         fontWeight: FontWeight.w500),
                                   ),
-                                  const SizedBox(height: 24),
+                                  const SizedBox(height: 32),
                                   FlatRoundIconButton(
                                     onTap: () {
                                       BlocProvider.of<ContactSyncBloc>(context)
@@ -62,7 +68,8 @@ class ContactSyncSetupScreen extends StatelessWidget {
                                       Icons.security,
                                       color: Colors.white,
                                     ),
-                                  )
+                                  ),
+                                  const SizedBox(height: 80),
                                 ],
                               ),
                             ),
@@ -70,84 +77,95 @@ class ContactSyncSetupScreen extends StatelessWidget {
                         }
 
                         if (state is ContactSyncRunningState) {
-                          return Container(
+                          return Center(
                             child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
                                 const SizedBox(height:80),
                                 Container(
-                                  height: 80,
-                                  width: 80,
+                                  height: 220,
+                                  width: 220,
                                   child: FlareActor(
                                     "assets/animations/logbook_spinner.flr",
                                     animation: "Untitled",
                                     isPaused: false,
                                   ),
                                 ),
-                                const SizedBox(height: 16),
-                                Text(
-                                  'Kontakte werden synchronisiert',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500),
+                                const SizedBox(height: 32),
+                                Container(
+                                  width: double.infinity,
+                                  child: Text(
+                                    'Deine Kontakte werden geladen',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.white70,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w600),
+                                  ),
                                 ),
+                                const SizedBox(height: 120),
                               ],
                             ),
                           );
                         }
 
                         if (state is ContactSyncDoneState) {
-                          return Column(
-                            children: <Widget>[
-                              const SizedBox(height:80),
-                              WithMaterialHero(
-                                tag: 'setup_title',
-                                child: Container(
-                                  width: double.infinity,
-                                  child: Text(
-                                    'Und Fertig',
-                                    style: TextStyle(
-                                        fontSize: 40,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(height: 24),
-                              Text(
-                                'Jetz kann es losgehen',
-                                style: TextStyle(
-                                    fontSize: 24,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                              const SizedBox(height: 24),
-                              Align(
-                                alignment: Alignment.bottomRight,
-                                child: Hero(
-                                  tag: 'setup_action_button',
-                                  child: FlatRoundIconButton(
-                                    onTap: () {
-                                      Navigator.of(context).pushReplacementNamed('/encounter');
-                                    },
-                                    child: Text('Abschließen'),
-                                    endIcon: Icon(
-                                      Icons.chevron_right,
-                                      color: Colors.white,
+                          return Center(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                const SizedBox(height:80),
+                                WithMaterialHero(
+                                  tag: 'setup_title',
+                                  child: Container(
+                                    width: double.infinity,
+                                    child: Text(
+                                      'Und Fertig',
+                                      style: TextStyle(
+                                          fontSize: 40,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(height: 24),
+                                Text(
+                                  'Jetz kann es losgehen',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                const SizedBox(height: 48),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Hero(
+                                    tag: 'setup_action_button',
+                                    child: FlatRoundIconButton(
+                                      onTap: () {
+                                        Navigator.of(context).pushReplacementNamed('/encounter');
+                                      },
+                                      child: Text('Abschließen'),
+                                      endIcon: Icon(
+                                        Icons.chevron_right,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 140),
+                              ],
+                            ),
                           );
                         }
 
                         return Container();
                       },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
